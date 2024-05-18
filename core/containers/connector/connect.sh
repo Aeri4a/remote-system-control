3106d7172f74c239e64dd024c5ba115687ca30bd
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2024-05-18 20:22:42 +0200

    script tiny refactor

--- core/containers/connector/connect.sh
+++ core/containers/connector/connect.sh
@@ -5,7 +5,10 @@ do
 
 echo "[Script info] - Initializing ssh connection..."
 
-ssh -T -i ./certificate/cert -p $SSH_PORT -o StrictHostKeychecking=no -R $OUT_API_PORT:$API_HOST:$IN_API_PORT $SSH_USER@$SSH_HOST \
+ssh -T -i ./certificate/cert \
+    -o StrictHostKeychecking=no \
+    -p $SSH_PORT \
+    -R $OUT_API_PORT:$API_HOST:$IN_API_PORT $SSH_USER@$SSH_HOST \
     "while sleep 10; do echo Connected - maintaing connection; done"
 
 echo "[Script info] - Connection closed or not initialized"
